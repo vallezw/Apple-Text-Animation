9c5026034be007187f7b6f142ac1c194dca4de11
Valentin Zwerschke <v.zwerschke@mail.de>
2021-01-24 13:20:44 +0100

    Add React Project and animation

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div class="animated-title">
+  <div class="text-top">
+    <div>
+      <span>Valentin</span>
+      <span>Zwerschke</span>
+    </div>
+  </div>
+  <div class="text-bottom">
+    <div>For the win!</div>
+  </div>
+</div>
+  );
+}
+
+export default App;
