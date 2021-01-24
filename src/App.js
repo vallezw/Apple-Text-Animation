269ac278bda296d5d5a47af0b357f00dab1d2443
Valentin Zwerschke <v.zwerschke@mail.de>
2021-01-24 13:22:06 +0100

    Remove junk files

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
-import logo from './logo.svg';
 import './App.scss';
 
 function App() {
   return (
     <div class="animated-title">
-  <div class="text-top">
-    <div>
-      <span>Valentin</span>
-      <span>Zwerschke</span>
-    </div>
+      <div class="text-top">
+        <div>
+          <span>Valentin</span>
+          <span>Zwerschke</span>
+        </div>
+      </div>
+        <div class="text-bottom">
+        <div>For the win!</div>
+      </div>
   </div>
-  <div class="text-bottom">
-    <div>For the win!</div>
-  </div>
-</div>
   );
 }
 
